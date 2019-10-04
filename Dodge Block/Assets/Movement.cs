415bfd04876b9cdf1b8e78ad07a5070219a61f2d
essarrgee <55785294+essarrgee@users.noreply.github.com>
2019-10-03 18:59:57 -0700

    Add Objects and Components

---
+++ Dodge Block/Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float speed;
+	
+	private RigidBody2D rb;
+	private Vector2 inputDir;
+	
+    // Start is called before the first frame update
+    void Awake()
+    {
+        speed = 15f;
+		
+		rb = this.GetComponent<RigidBody2D>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputDir.x = Input.GetAxisRaw("Horizontal");
+		inputDir.y = Input.GetAxisRaw("Vertical");
+		inputDir = inputDir.normalized;
+		
+		rb.MovePosition(inputDir);
+    }
+}
